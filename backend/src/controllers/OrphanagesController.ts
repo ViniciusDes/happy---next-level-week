c2da1f007c1bad9af702e49e54de93f0d049985e
Vinicius <viniciussilva9200@gmail.com>
2020-10-24 10:55:03 -0300

    Criação API REST com NodeJS na pasta Backend com os metodos prontos e CORS liberado

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -1,21 +1,22 @@
 // import { create } from "domain";
-import {request, Request, Response} from "express";
+import {Request, Response} from "express";
 import {getRepository} from "typeorm"
 import Orphanage from "../models/Orphanage";
 import OrphanageView from "../views/Orphanages_views";
-
+import * as Yup from "yup";
 
 export default{
   
-    async show(req: Request, res: Response){
+    async show(req: Request, res: Response){//so retorna um orfanato
         const orphanagesRepository = getRepository(Orphanage);
-        const {id} = req.params;
+        const {id} = req.params;//pegando a id do parametro passado na requisição
         const orphanage = await orphanagesRepository.findOneOrFail(id, {
             relations: ["images"]
         });
  
         return res.json(OrphanageView.render(orphanage));
     },
+
     async index(req: Request, res: Response){
         const orphanagesRepository = getRepository(Orphanage);
         const orphanages = await orphanagesRepository.find({
@@ -23,7 +24,9 @@ export default{
         });
 
         return res.json(OrphanageView.renderMany(orphanages));
+        
     },
+
     async create(req: Request, res: Response){
         const {
             name,
@@ -42,7 +45,7 @@ export default{
         } )
         console.log(images);
 
-        const orphanage = orphanagesRepository.create({
+        const data = {
             name,
             latitude,
             longitude,
@@ -50,7 +53,26 @@ export default{
             opening_hours,
             instructions,
             images
-        });
+        }
+        
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            latitude: Yup.number().required(),
+            longitude: Yup.number().required(),
+            about: Yup.string().required().max(300),
+            instructions: Yup.string().required("instrução é obrigadore"),
+            opening_hours: Yup.string().required(),
+            open_on_weekends: Yup.boolean().required(),
+            images: Yup.array(
+                Yup.object().shape({
+                path: Yup.string().required()
+            }))
+        })
+
+        await schema.validate(data, {
+            abortEarly: false,//para retornar o erro de todos os campos e nao somente de um em um
+        })
+        const orphanage = orphanagesRepository.create(data);
     
         await orphanagesRepository.save(orphanage);
         return res.json({message: "Dados salvos com sucesso"})
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,17 @@
-import express, { response } from "express";
-import routes from "./routes"
-import './database/conenection';
+import express from "express";
 import path from "path";
-
-
+import "express-async-errors";
+import routes from "./routes";
+import './database/conenection';
+import errorHandler from "./errors/handler";
+import {cors} from "cors"
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.use("/uploads", express.static(path.join(__dirname, '..', 'uploads')))
+app.use("/uploads", express.static(path.join(__dirname, '..', 'uploads')))//construindo o endereço para acessar arquivos 
+app.use(errorHandler);
+
 
 app.listen(3333); 
